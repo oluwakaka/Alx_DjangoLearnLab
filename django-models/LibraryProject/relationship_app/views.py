9edec00bf7a7eb18eb03ea150838fdd1bd19c064
Kaka <kakaolalekan@gmail.com>
2025-08-08 15:50:35 +0100

    Fix views and templates to match Alx checker requirements

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,8 +4,8 @@ from .models import Library, Book
 
 # Function-based view
 def list_books(request):
-    books = Book.objects.select_related('author').all()
-    return render(request, 'list_books.html', {'books': books})
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # Class-based view
 class LibraryDetailView(DetailView):
