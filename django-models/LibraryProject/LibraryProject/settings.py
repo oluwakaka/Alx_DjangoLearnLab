0da89d899d8f69d77ba2be168fc602688013e5f9
Kaka <kakaolalekan@gmail.com>
2025-08-08 15:40:10 +0100

    Fix class-based view and templates

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'LibraryProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'relationship_app', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,14 @@
-from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
-from .models import Book, Library
+from django.shortcuts import render
+from .models import Library, Book
 
-# Function-Based View
+# Function-based view
 def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    books = Book.objects.select_related('author').all()
+    return render(request, 'list_books.html', {'books': books})
 
-# Class-Based View
+# Class-based view
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'
+    template_name = 'library_detail.html'
     context_object_name = 'library'
-
