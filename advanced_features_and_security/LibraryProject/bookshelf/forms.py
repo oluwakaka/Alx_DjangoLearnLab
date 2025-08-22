96b966fa0e8d99aab78de61ef373d89720a67d9b
Kaka <kakaolalekan@gmail.com>
2025-08-22 20:37:34 +0100

    feat: add security settings, CSRF-protected forms, safe views and templates

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=200)
+    author = forms.CharField(max_length=100)
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm
+from django.views.decorators.csrf import csrf_protect
+
+
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
     books = Book.objects.all()
@@ -18,3 +22,16 @@ def delete_book(request):
 @permission_required('bookshelf.can_view', raise_exception=True)
 def view_book(request):
     return HttpResponse("You can view books.")
+
+@csrf_protect
+def form_example(request):
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Safe handling of user input
+            title = form.cleaned_data['title']
+            author = form.cleaned_data['author']
+            Book.objects.create(title=title, author=author, published_date="2000-01-01")
+    else:
+        form = ExampleForm()
+    return render(request, "bookshelf/form_example.html", {"form": form})
