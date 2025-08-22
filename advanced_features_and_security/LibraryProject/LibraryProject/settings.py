d9ddcb20a7abcb95f7aa6aa35a19a6d5dfcb5c7e
Kaka <kakaolalekan@gmail.com>
2025-08-22 21:22:54 +0100

    feat: enforce HTTPS, HSTS, secure cookies, and headers

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -32,6 +32,12 @@ SESSION_COOKIE_SECURE = True
 # Optional: Redirect HTTP to HTTPS
 SECURE_SSL_REDIRECT = True
 
+# HTTP Strict Transport Security (HSTS)
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+
 # ==============================
 # Application definition
 # ==============================
