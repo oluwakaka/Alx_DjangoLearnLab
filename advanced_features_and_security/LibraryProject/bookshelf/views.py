46bf067739e6758b6dab3ce45ce78e11d179819d
Kaka <kakaolalekan@gmail.com>
2025-08-22 20:09:22 +0100

    feat: add CustomUser model, Book model with custom permissions, views and templates with permission decorators

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import permission_required
-
+from .models import Book
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
-    return HttpResponse("You can create a book.")
+    books = Book.objects.all()
+    return render(request, "bookshelf/book_list.html", {"books": books})
 
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def edit_book(request):
